319dd6039a50e2065e45f7d80d11b5816370b1c1
lab@techmoon.in <lab@techmoon.in>
2018-09-13 19:20:36 +0530

    up

--- js/youtube.js
+++ js/youtube.js
@@ -187,7 +187,7 @@ lastnotificationtext="<span>"+notificationbody+"</span>";
   setInterval(function(){
       $(".close-padding.condensed.contains-svg,.iv-promo-close").click();
       $(".ytp-ad-skip-button,.videoAdUiSkipButton").click()
-          $(".ytp-ad-overlay-close-button").click()
+          $(".ytp-ad-overlay-close-button").click();
     $(".annotation-close-button").click();
   if($(".ytp-right-controls .customnotificationactive").length==0){
     $(".ytp-right-controls").prepend('<button title="View Sub Titles as Notification" class="ytp-subtitles-button ytp-button customnotificationactive" style="" aria-pressed="'+notificationactive+'"><span style="  height: 100%;  width: 100%;  display: block; float: left;  margin: -1px 1px 0px 0px;  text-align: center;">NT</span></button>');
